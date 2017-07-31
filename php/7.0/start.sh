7cbe1291b2e14042564dde138790569f552b82b5
José Vieira <hello@josevieira.me>
2017-07-31 17:59:13 -0300

    enable xdebug remote_connect_back option

--- php/7.0/start.sh
+++ php/7.0/start.sh
@@ -11,6 +11,7 @@ if [[ $XDEBUG_ENABLED == true ]]; then
     echo "xdebug.remote_host=`/sbin/ip route|awk '/default/ { print $3 }'`" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.remote_port=9000" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.scream=0" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_connect_back=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null    
     echo "xdebug.cli_color=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.show_local_vars=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
     echo 'xdebug.idekey = "generation"' | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
